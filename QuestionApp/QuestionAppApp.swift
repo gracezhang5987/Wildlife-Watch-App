fbba1b05030a86c51f196d6af0c26116ce6718f2
Grace <gracezhang5987@gmail.com>
2024-07-30 10:10:06 -0400

    Initial Commit

---
+++ QuestionApp/QuestionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionAppApp.swift
+//  QuestionApp
+//
+//  Created by Jieping Li on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
